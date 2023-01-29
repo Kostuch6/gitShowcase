f749e8fdfb4dc86f09e17f11d0c30f47ccd5207c
Kostuch6 <lukas.marchel@gmail.com>
2023-01-29 17:37:56 +0100

    Initial commit with starting code

---
+++ src/main/java/com/example/gitshowcase/GitshowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitshowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitshowcaseApplication.class, args);
+	}
+
+}
